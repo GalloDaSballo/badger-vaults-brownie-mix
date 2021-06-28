1bec4289c6245d15bbdec2f455019fa3783aacec
Entreprenerd <valeranijr@gmail.com>
2021-06-28 15:05:35 +0200

    fix: code compiles, 3 implementation test fails correctly, issue with conftest for badger

--- tests/badger/conftest.py
+++ tests/badger/conftest.py
@@ -1,15 +1,10 @@
 from brownie import *
 from config import (
-  BADGER_DEV_MULTISIG,
   WANT,
   LP_COMPONENT,
   REWARD_TOKEN,
-  DEFAULT_GOV_PERFORMANCE_FEE,
-  DEFAULT_PERFORMANCE_FEE,
-  DEFAULT_WITHDRAWAL_FEE
 )
-from dotmap import DotMap
-from scripts.deploy import deploy
+from scripts.badger.deploy import deploy
 import pytest
 
 @pytest.fixture
